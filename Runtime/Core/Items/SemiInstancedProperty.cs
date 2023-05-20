84f1ad30a2c07965e075cdc04ed76adf420d56f8
Robert Christensen <robert.christensen@mymail.champlain.edu>
2023-05-20 11:19:36 -0500

    Stubs for semi-instanced properties

---
+++ Runtime/Core/Items/SemiInstancedProperty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[Serializable]
+public abstract class SemiInstancedProperty : ItemProperty //Shared part
+{
+    public abstract class InstancedData<T> : ItemInstanceProperty where T : SemiInstancedProperty //Instanced part
+    {
+        protected T shared { get; private set; }
+
+        protected InstancedData(T shared)
+        {
+            this.shared = shared;
+        }
+    }
+}
