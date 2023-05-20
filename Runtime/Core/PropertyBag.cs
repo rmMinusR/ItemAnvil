4816f07ee12eea5d75f73b5a3f6642e1e7e4c2ab
Robert Christensen <robert.christensen@mymail.champlain.edu>
2023-05-20 12:06:25 -0500

    Made PropertyBag a proper set

--- Runtime/Core/PropertyBag.cs
+++ Runtime/Core/PropertyBag.cs
@@ -6,12 +6,12 @@ using System.Reflection;
 using UnityEngine;
 
 /// <summary>
-/// A unified way to store properties. In general, only one property of each type may exist per bag. Used in Items and ItemStacks.
+/// A unified way to store properties. In general, only one property of each type may exist per bag. Used in Items and ItemStacks to store ItemProperties and ItemInstanceProperties.
 /// </summary>
 /// <typeparam name="TBase">Base class of all properties</typeparam>
 [Serializable]
-public sealed class PropertyBag<TBase> : IEnumerable<TBase>, ICloneable //Should prob be an ISet, but that's a lot of work. TODO.
-    where TBase : class, ICloneable // Exclude structs and POD, since they don't have inheritance. ICloneable to fix inappropriate ref sharing
+public sealed class PropertyBag<TBase> : ISet<TBase>, ICloneable
+                          where TBase : class, ICloneable // Exclude structs and POD, since they don't have inheritance. ICloneable to fix inappropriate ref sharing
 {
     [Serializable]
     private class Container : ISerializationCallbackReceiver // Serialization helper
@@ -31,6 +31,9 @@ public sealed class PropertyBag<TBase> : IEnumerable<TBase>, ICloneable //Should
     }
     [SerializeField] private List<Container> contents;
 
+    public int Count => contents.Count;
+    public bool IsReadOnly => false;
+
     public T Add<T>() where T : TBase, new()
     {
         if (!contents.Any(i => i.value is T))
@@ -40,16 +43,21 @@ public sealed class PropertyBag<TBase> : IEnumerable<TBase>, ICloneable //Should
             contents.Add(new Container() { value = obj });
             return obj;
         }
-        else throw new InvalidOperationException($"Tried to add a {typeof(T).Name}, but it already exists!");
+        else throw new InvalidOperationException($"Tried to add a {typeof(T).Name}, but one already exists!");
     }
     
-    public void Add<T>(T toAdd) where T : TBase
+    public bool Add(TBase toAdd)
     {
-        if (!contents.Any(i => i.value is T))
+        if (!contents.Any(i => i.value.GetType() == toAdd.GetType()))
         {
             contents.Add(new Container() { value = toAdd });
+            return true;
         }
-        else throw new InvalidOperationException($"Tried to add a {typeof(T).Name}, but it already exists!");
+        else
+        {
+            Debug.LogWarning($"Tried to add a {toAdd.GetType().Name}, but one already exists!");
+            return false;
+        }
     }
 
     public T Get<T>() where T : TBase
@@ -100,5 +108,53 @@ public sealed class PropertyBag<TBase> : IEnumerable<TBase>, ICloneable //Should
     public object Clone()
     {
         return MemberwiseClone(); //FIXME deep clone, this will share property instances!
+    }
+
+    /////////////// ISet impl ///////////////
+
+    public void ExceptWith   (IEnumerable<TBase> other) => contents.RemoveAll(c =>  other.Contains(c.value));
+    public void IntersectWith(IEnumerable<TBase> other) => contents.RemoveAll(c => !other.Contains(c.value));
+
+    public bool IsProperSubsetOf  (IEnumerable<TBase> other) => IsSubsetOf  (other) && Count != other.Count();
+    public bool IsProperSupersetOf(IEnumerable<TBase> other) => IsSupersetOf(other) && Count != other.Count();
+
+    public bool IsSubsetOf  (IEnumerable<TBase> other) => this.All(i => other.Contains(i));
+    public bool IsSupersetOf(IEnumerable<TBase> other) => other.All(i => this.Contains(i));
+
+    public bool Overlaps(IEnumerable<TBase> other)
+    {
+        foreach (TBase i in other) if (this .Contains(i)) return true;
+        foreach (TBase i in this ) if (other.Contains(i)) return true;
+        return false;
+    }
+
+    public bool SetEquals(IEnumerable<TBase> other) => IsSubsetOf(other) && IsSupersetOf(other);
+
+    public void SymmetricExceptWith(IEnumerable<TBase> other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UnionWith(IEnumerable<TBase> other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(TBase item) => contents.Any(i => i.value == item);
+    public void Clear() => contents.Clear();
+
+    public void CopyTo(TBase[] array, int arrayIndex)
+    {
+        if (array.Length < contents.Count+arrayIndex) throw new IndexOutOfRangeException("Not enough space in destination array");
+
+        for (int i = 0; i < contents.Count; ++i)
+        {
+            array[i+arrayIndex] = contents[i].value;
+        }
+    }
+
+    void ICollection<TBase>.Add(TBase item)
+    {
+        this.Add(item); // Defer to main impl
     }
 }
